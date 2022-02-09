fc34be597606c20323811d9fc940c410d3a1fa61
CJonesTSU <cjone141@my.tnstate.edu>
2022-02-09 20:54:49 +0000

    String object without constructor call

---
+++ Week3/0209/StringConstructorDemo.java
@@ -0,0 +1,9 @@
+public class StringConstructorDemo{
+  public static void main(String[] args){
+    String myString = "Hi there!";
+    String my2ndString = new String("This is my text");
+    
+    System.out.println(myString);
+    System.out.println(my2ndString);
+  }
+}

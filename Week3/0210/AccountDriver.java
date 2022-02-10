bbb735fc79a7a4aa310d8666bfb4309786eeccd2
CJonesTSU <cjone141@my.tnstate.edu>
2022-02-10 16:48:25 +0000

    Cleaned up output

--- Week3/0210/AccountDriver.java
+++ Week3/0210/AccountDriver.java
@@ -8,7 +8,7 @@ public class AccountDriver{
     System.out.println("acc2 initial balance is: " + acc2.getBalance());
     System.out.println("acc3 initial balance is: " + acc3.getBalance());
     
-    
+    System.out.println();
     System.out.println("Testing deposit method");
     
     acc1.deposit(99.23);
@@ -16,14 +16,14 @@ public class AccountDriver{
     System.out.println("acc1 deposited $99.23 account balance is: " + acc1.getBalance());
     System.out.println("acc2 deposited $15.75 balance is: " + acc2.getBalance());    
     
-    System.out.println("Testing withdraw method");
+    System.out.println("\nTesting withdraw method");
     acc2.withdraw(2.50);
     acc3.withdraw("8.25");
     
     System.out.println("acc2 withdrew $2.50 account balance is: " + acc2.getBalance());
     System.out.println("acc3 withdrew $8.25 balance is: " + acc3.getBalance());
     
-    System.out.println("Testing set balance methods");
+    System.out.println("\nTesting set balance methods");
     acc1.setBalance(100.01);
     acc3.setBalance("5000.50");
     

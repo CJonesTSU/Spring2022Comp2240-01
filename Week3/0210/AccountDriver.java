06c48429026ec330b814befb952615e097cb4598
CJonesTSU <cjone141@my.tnstate.edu>
2022-02-10 16:24:03 +0000

    Created three construtors

--- Week3/0210/AccountDriver.java
+++ Week3/0210/AccountDriver.java
@@ -1,5 +1,7 @@
 public class AccountDriver{
   public static void main(String[] args){
-    BankAccount acc = new BankAccount();
-  }
+    BankAccount acc1 = new BankAccount();
+    BankAccount acc2 = new BankAccount(1.75);
+    BankAccount acc3 = new BankAccount("55.23");
+   }
 }
--- Week3/0210/BankAccount.java
+++ Week3/0210/BankAccount.java
@@ -1,3 +1,16 @@
 public class BankAccount{
   private double balance;
+  
+  // Constructors
+  public BankAccount(){
+    balance = 0.0;
+  }
+  
+  public BankAccount(double startBalance){
+    balance = startBalance;
+  }
+  
+  public BankAccount(String startBalance){
+    balance = Double.parseDouble(startBalance);
+  }
 }
